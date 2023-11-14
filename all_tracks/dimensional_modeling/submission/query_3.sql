9d285d308b523e14311f6ab424004a6f4e5901b9
Cristopher Freitas <cristopher@voxy.com>
2023-11-13 23:58:08 -0300

    Push the schema for the `actors_history_scd` table.

---
+++ all_tracks/dimensional_modeling/submission/query_3.sql
@@ -0,0 +1,9 @@
+CREATE TABLE actors_history_scd (
+    actor         VARCHAR,
+    actor_id      VARCHAR,
+    quality_class VARCHAR,
+    is_active     BOOLEAN,
+    start_date  INTEGER,
+    end_date  INTEGER
+)
+
