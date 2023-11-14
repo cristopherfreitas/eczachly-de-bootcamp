db3ab2c603cc2ca80bd028d4d77e4fe5360990a8
Cristopher Freitas <cristopher@voxy.com>
2023-11-13 23:57:44 -0300

    Remove the V2 from query 2 and fix the quality change column.

--- all_tracks/dimensional_modeling/submission/query_2.sql
+++ all_tracks/dimensional_modeling/submission/query_2.sql
@@ -32,7 +32,7 @@ cumulative_table AS (
             WHEN ty.films IS NOT NULL AND ly.films IS NOT NULL THEN
                 ty.films || ly.films
         END AS films,
-        ty.quality_class AS quality_class,
+        COALESCE(ty.quality_class, ly.quality_class) AS quality_class,
         ty.year is not null as is_active,
         COALESCE(ty.year, ly.current_year + 1) as current_year
     FROM last_year ly
@@ -40,47 +40,4 @@ cumulative_table AS (
     ON ly.actor_id = ty.actor_id
 )
 SELECT * FROM cumulative_table
-GROUP BY actor, actor_id, films, quality_class, is_active, current_year
-
--- v2, window functions and row after
-WITH last_year AS (
-    SELECT *
-    FROM cristophersfr.actors
-    WHERE current_year = 1934
-),
-this_year AS (
-    SELECT *
-    FROM bootcamp.actor_films
-    WHERE year = 1935
-),
-cumulative_table AS (
-    SELECT
-        COALESCE(ly.actor, ty.actor) AS actor,
-        COALESCE(ly.actor_id, ty.actor_id) AS actor_id,
-        CASE
-            -- If the actor has no new films, use the history.
-            WHEN ty.film IS NULL THEN ly.films
-            -- If the actor has no history, create an array with only the new one.
-            WHEN ly.films IS NULL AND ty.film IS NOT NULL THEN
-                array_agg(ROW(ty.film, ty.film_id, ty.votes, ty.rating)) OVER (PARTITION BY ty.actor_id)
-            -- If the actor has both history and new films, append the new ones.
-            WHEN ty.film IS NOT NULL AND ly.films IS NOT NULL THEN
-                array_agg(ROW(ty.film, ty.film_id, ty.votes, ty.rating)) OVER (PARTITION BY ty.actor_id) || ly.films
-        END AS films,
-        -- The quality class is the average rating of the actor's films.
-        CASE
-            WHEN AVG(rating) OVER (PARTITION BY ty.actor_id) > 8 THEN 'star'
-            WHEN AVG(rating) OVER (PARTITION BY ty.actor_id) > 7 THEN 'good'
-            WHEN AVG(rating) OVER (PARTITION BY ty.actor_id) > 6 THEN 'average'
-            ELSE 'bad'
-        END AS quality_class,
-        ty.year is not null as is_active,
-        COALESCE(ty.year, ly.current_year + 1) as current_year
-    FROM last_year ly
-    FULL OUTER JOIN this_year ty
-    ON ly.actor_id = ty.actor_id
-)
-SELECT
-    *
-FROM cumulative_table
 GROUP BY actor, actor_id, films, quality_class, is_active, current_year
